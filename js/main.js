0be076ddf4ee4bc0dc053064dec8d2e4cd5ebf21
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-03-07 14:29:11 +0530

    'first'

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(".sidemenu-toggler").click(function () {
+    $(".sidemenu").addClass("active");
+    $(".line-top").addClass("active");
+    $(".line-bottom").addClass("active");
+    $(".close").addClass("active");
+  });
+  $(".close").click(function () {
+    $(this).removeClass("active");
+    $(".sidemenu").removeClass("active");
+    $(".line-top").removeClass("active");
+    $(".line-bottom").removeClass("active");
+  });
+
+  $(window).scroll(function () {
+    var sc = $(window).scrollTop();
+    if (sc > 0) {
+      $(".header").addClass(".sticky");
+    } else {
+      $(".header").removeClass(".sticky");
+    }
+  });
+
+  $(window).on("load", function () {
+    var $container = $(".portfolioContainer");
+    $container.isotope({
+      filter: "*",
+      animationOptions: {
+        queue: true,
+      },
+    });
+  });
+});
