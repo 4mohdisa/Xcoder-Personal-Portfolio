70d33691a8ffdeda5eb5af066eb2442c18bec422
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-03-09 00:53:03 +0530

    works

---
+++ js/icon.js
@@ -0,0 +1,56 @@
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/ecommerce.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime1"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/AppDev.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime2"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/Graphic.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime3"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/WebDesign.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime4"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/Ui.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime5"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/GameDev.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime6"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/Marketing.json",
+});
+var anime = bodymovin.loadAnimation({
+  container: document.getElementById("anime7"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/json/Editing.json",
+});
