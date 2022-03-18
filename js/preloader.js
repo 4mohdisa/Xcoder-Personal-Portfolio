0d29df2a3282007e984bbb01dc8e0cab6e71ef48
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-03-19 01:16:24 +0530

    make it better

---
+++ js/preloader.js
@@ -0,0 +1,4 @@
+$(window).on("load", function () {
+  $("#preloader").delay(1550).fadeOut("slow");
+  $("body").delay(1550).css({ overflow: "visible" });
+});
---
+++ js/reveal.js
@@ -0,0 +1,30 @@
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+ScrollReveal().reveal(
+  ".Big-Text, .secondary-title, .tag0, .carD1, .mh-professional-skill, .bar2",
+  {
+    delay: 500,
+    origin: "left",
+  }
+);
+ScrollReveal().reveal(".section-paragraph, .carD2", {
+  delay: 400,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".BTTN, .carD3, .pro-skill, .bar1", {
+  delay: 400,
+  origin: "right",
+});
+ScrollReveal().reveal(".Man, .tag1", {
+  delay: 300,
+  origin: "top",
+});
+
+var image = document.getElementsByClassName("thumbnail");
+new simpleParallax(image, {
+  orientation: "right",
+});
